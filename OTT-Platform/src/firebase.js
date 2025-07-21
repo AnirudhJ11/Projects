5d15cdca53fb5aaedb7d94d49373edde4bec40de
AnirudhJ11 <vishnuj7276@gmail.com>
2025-07-21 13:46:28 +0530

    OTT

---
+++ OTT-Platform/src/firebase.js
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, getFirestore } from "firebase/firestore/lite";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  /**API DETAILS */
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name,email,password)=>{
+    try {
+        const res = await createUserWithEmailAndPassword(auth,email,password);
+        const user = res.user;
+        await addDoc(collection(db,"user"),{
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const login = async (email,password)=>{
+    try {
+        await signInWithEmailAndPassword(auth, email,password)
+    } catch (error) {
+        console.log(error)
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const logout = ()=>{
+    signOut(auth); 
+};
+
+export {auth,db,login,signup,logout};
